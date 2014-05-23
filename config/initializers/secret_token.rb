9fe9f59742dfa782bff4d80927d0d4a1054c5cbf
Michael Todd <mikejtodd@gmail.com>
2014-05-22 21:28:26 -0500

    Files setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Donorator::Application.config.secret_token = 'e2a7359c4a285177a12b7b2752715bfb43982313d57e25037ebeaae8d5db0f52b4182df2cf47a68f7c4201b59c93a39f071f747d730dcba43322123dc45caab0'
